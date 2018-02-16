46f8e6ebbae1c1b62ad895c1703eced7c62e6456
Olla Tech <olla.code@gmail.com>
2018-02-16 20:02:25 +0700

    global update

---
+++ pack/createElement.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = createElement;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function createElement(_ref) {
+  var componentObj = _ref.componentObj,
+      props = _ref.props,
+      context = _ref.context;
+
+  return _react2.default.createElement(componentObj, props);
+}
